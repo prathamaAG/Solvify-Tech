382efb4abff38cff2dd6119b43e17ca433081d1a
prathamaAG <pratham.s.aghera@gmail.com>
2026-04-12 00:41:59 +0530

    changed in cors origin

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL, 
+  origin: [
+    process.env.CLIENT_URL, 
+    'http://localhost:5173', 
+    'http://localhost:3000'
+  ].filter(Boolean), 
   methods: 'GET,POST,PUT,DELETE',
   credentials: true 
 }));
